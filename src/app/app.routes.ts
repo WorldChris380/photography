dd9c98efeb54d3751145004bf89e489cc2bbbf92
WorldChris380 <christian.boehme.private@gmail.com>
2025-08-07 22:29:20 +0200

    first steps again

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
+import { Main } from './main/main';
+
 
 export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
+import { Main } from './main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Main, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
